31674778b0a94fc625c8d3c0f61a6aa475db254c
Quentin Peten <quentin.peten@lesniquedouilles.eu>
2025-07-27 17:47:04 +0200

    Initial commit

---
+++ vehicle/OVMS.V3/components/vehicle_vwid3/src/vehicle_vwid3.h
@@ -0,0 +1,48 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2025       Quentin Peten
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#ifndef __VEHICLE_VWID3_H__
+#define __VEHICLE_VWID3_H__
+
+#include "vehicle.h"
+
+using namespace std;
+
+class OvmsVehicleVWID3 : public OvmsVehicle
+  {
+  public:
+    OvmsVehicleVWID3();
+    ~OvmsVehicleVWID3();
+
+  protected:
+    void IncomingPollReply(const OvmsPoller::poll_job_t &job, uint8_t* data, uint8_t length) override;
+
+  protected:
+    char m_vin[18];
+  };
+
+#endif //#ifndef __VEHICLE_VWID3_H__
